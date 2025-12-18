c6acbaaec6fac9533d44fb854bf268915a78ba0f
Witwin12 <kunwin12@gmail.com>
2025-12-18 23:30:11 +0700

    setup project

---
+++ realtime-notification/src/test/java/com/example/realtime_notification/RealtimeNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.realtime_notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtimeNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
